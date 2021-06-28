74e752b7c02353ea353340723d4af5a76504c4a3
wal33d006 <waleed_arshad006@hotmail.com>
2021-06-28 13:24:58 +0500

    Add localizations support

--- lib/app_widget.dart
+++ lib/app_widget.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_app/navigation/app_navigator.dart';
 import 'package:flutter_app/utils/app_theme.dart';
 import 'package:flutter_app/views/mnemonic_onboarding.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 class AppWidget extends StatelessWidget {
   const AppWidget();
@@ -14,6 +16,13 @@ class AppWidget extends StatelessWidget {
       theme: AppTheme.buildAppTheme(),
       home: MnemonicOnboarding(),
       navigatorKey: AppNavigator.navigatorKey,
+      localizationsDelegates: const [
+        AppLocalizations.delegate,
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      supportedLocales: const [Locale('en', '')],
     );
   }
 }
