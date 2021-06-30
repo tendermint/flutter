c58a100c73b1c519cae4157aac68a2a56126dee9
Andrzej Chmielewski <andrzejchm@users.noreply.github.com>
2021-06-30 14:03:25 +0200

    feat: Initial packages structure (#60)

--- lib/helpers/mnemonic_generator.dart
+++
@@ -1,5 +0,0 @@
-import 'package:bip39/bip39.dart' as bip39;
-
-abstract class MnemonicGenerator {
-  static String generateMnemonic() => bip39.generateMnemonic(strength: 256);
-}
---
+++ packages/transaction_signing_gateway/lib/signing_gateway.dart
@@ -0,0 +1,4 @@
+library transaction_signing_gateway;
+
+export 'package:transaction_signing_gateway/transaction_signer.dart';
+export 'package:transaction_signing_gateway/transaction_signing_gateway.dart';
