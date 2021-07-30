6de24f211993c15a8321787d5cc2450349b0e371
Andrzej Chmielewski <andrzejchm@gmail.com>
2021-07-30 09:42:30 +0300

    remove more idea files

--- .idea/fileTemplates/useCase.dart
+++
@@ -1,9 +0,0 @@
-#set( $ClassName = ${StringUtils.removeAndHump(${NAME}, "_")})
-import 'package:dartz/dartz.dart';
-import 'package:flutter_app/domain/model/failures/${NAME}_failure.dart';
-
-class ${ClassName}UseCase {
-  Future<Either<${ClassName}Failure, R>> execute() async {
-    
-  }
-}
