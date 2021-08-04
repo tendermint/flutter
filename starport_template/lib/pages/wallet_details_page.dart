d974f4db6b705e46e613180f10fb795a5e8f8091
Andrzej Chmielewski <andrzejchm@users.noreply.github.com>
2021-08-04 09:41:25 +0200

    feat: template - wallets list page (#76)

---
+++ starport_template/lib/pages/wallet_details_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+// TODO
+class WalletDetailsPage extends StatelessWidget {
+  const WalletDetailsPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text("Wallet Details Page"),
+      ),
+    );
+  }
+}
