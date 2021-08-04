04fffdf4565c6f8cde12b9609be6f2fc509ecd6f
Andrzej Chmielewski <andrzejchm@gmail.com>
2021-08-04 09:21:02 +0200

    add empty list message

--- packages/cosmos_ui_components/lib/cosmos_ui_components.dart
+++ packages/cosmos_ui_components/lib/cosmos_ui_components.dart
@@ -5,6 +5,7 @@ export 'components/content_state_switcher.dart';
 export 'components/cosmos_app_bar.dart';
 export 'components/cosmos_elevated_button.dart';
 export 'components/cosmos_error_view.dart';
+export 'components/empty_list_message.dart';
 export 'components/mnemonic_choice_chip.dart';
 export 'components/mnemonic_words_grid.dart';
 export 'components/template/cosmos_wallet_list_item.dart';
