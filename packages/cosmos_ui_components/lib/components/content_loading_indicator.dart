72317e827453a34ef12b75ef0e2db054393d1466
Andrzej Chmielewski <andrzejchm@gmail.com>
2021-09-29 13:14:01 +0200

    packages improvements

--- packages/cosmos_ui_components/lib/components/content_loading_indicator.dart
+++ packages/cosmos_ui_components/lib/components/content_loading_indicator.dart
@@ -1,12 +1,25 @@
+import 'package:cosmos_ui_components/cosmos_ui_components.dart';
 import 'package:flutter/material.dart';
 
 class ContentLoadingIndicator extends StatelessWidget {
+  final String message;
+
   const ContentLoadingIndicator({
     Key? key,
+    this.message = "",
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Center(child: CircularProgressIndicator());
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const CircularProgressIndicator(),
+          const SizedBox(height: CosmosAppTheme.spacingM),
+          if (message.isNotEmpty) Text(message),
+        ],
+      ),
+    );
   }
 }
--- packages/cosmos_utils/lib/mnemonic.dart
+++ packages/cosmos_utils/lib/mnemonic.dart
@@ -1,3 +1,6 @@
 import 'package:bip39/bip39.dart' as bip39;
+import 'package:flutter/foundation.dart';
 
-String generateMnemonic({int strength = 256}) => bip39.generateMnemonic(strength: strength);
+String _generateMnemonic(int strength) => bip39.generateMnemonic(strength: strength);
+
+Future<String> generateMnemonic({int strength = 256}) => compute(_generateMnemonic, strength);
