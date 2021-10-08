3fe5a1016cc3650b0d8d322910aedbeeef0d4ac2
wal33d006 <waleed_arshad006@hotmail.com>
2021-10-08 21:37:03 +0500

    Fix color appearances for cosmos_outline_button.dart

--- packages/cosmos_ui_components/lib/components/cosmos_outline_button.dart
+++ packages/cosmos_ui_components/lib/components/cosmos_outline_button.dart
@@ -17,6 +17,10 @@ class CosmosOutlineButton extends StatelessWidget {
   Widget build(BuildContext context) {
     return OutlinedButton(
       onPressed: onTap,
+      style: OutlinedButton.styleFrom(
+        primary: Theme.of(context).colorScheme.secondary,
+        side: BorderSide(color: Theme.of(context).colorScheme.secondary),
+      ),
       child: Row(
         mainAxisSize: MainAxisSize.min,
         children: [
