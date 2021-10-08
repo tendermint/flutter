d80ac8a5e88a8e6da7addc4e6bc4ff00ace2f8fc
wal33d006 <waleed_arshad006@hotmail.com>
2021-10-08 18:11:10 +0500

    Add styling to cosmos_elevated_button.dart according to the UI in whimsical

--- packages/cosmos_ui_components/lib/components/cosmos_elevated_button.dart
+++ packages/cosmos_ui_components/lib/components/cosmos_elevated_button.dart
@@ -5,18 +5,29 @@ class CosmosElevatedButton extends StatelessWidget {
   final VoidCallback? onTap;
   final String text;
   final Widget? suffixIcon;
+  final bool outlined;
 
   const CosmosElevatedButton({
     Key? key,
     this.onTap,
     this.text = "",
     this.suffixIcon,
+    this.outlined = false,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final secondaryColor = Theme.of(context).colorScheme.secondary;
+    final primaryColor = Theme.of(context).colorScheme.primary;
     return ElevatedButton(
       onPressed: onTap,
+      style: ElevatedButton.styleFrom(
+        primary: outlined ? primaryColor : secondaryColor,
+        onPrimary: outlined ? secondaryColor : primaryColor,
+        side: BorderSide(
+          color: outlined ? secondaryColor : primaryColor,
+        ),
+      ),
       child: Row(
         mainAxisSize: MainAxisSize.min,
         children: [
