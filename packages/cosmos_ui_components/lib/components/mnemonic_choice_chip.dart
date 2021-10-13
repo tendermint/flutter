9a9a4c66312af454d284574f3412393643d4e4a0
Andrzej Chmielewski <andrzejchm@gmail.com>
2021-10-13 15:14:37 +0200

    improve mnemonic chip grid

--- packages/cosmos_ui_components/lib/components/mnemonic_choice_chip.dart
+++ packages/cosmos_ui_components/lib/components/mnemonic_choice_chip.dart
@@ -1,32 +1,57 @@
+import 'package:cosmos_ui_components/cosmos_app_theme.dart';
 import 'package:flutter/material.dart';
 
 class MnemonicChoiceChip extends StatelessWidget {
   final int index;
   final String word;
+  final bool showIndex;
+  final VoidCallback? onTap;
 
   const MnemonicChoiceChip({
     Key? key,
     required this.index,
     required this.word,
+    this.showIndex = true,
+    this.onTap,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return ChoiceChip(
-      label: Text(word),
-      selected: true,
-      avatar: CircleAvatar(
-        backgroundColor: Theme.of(context).primaryColorLight,
-        child: Text(
-          "${index + 1}",
-          style: const TextStyle(fontSize: 12),
+    final textTheme = Theme.of(context).textTheme;
+    final lightInactive = CosmosColors.lightInactive;
+    final lightDivider = CosmosColors.lightDivider;
+    return InkWell(
+      onTap: onTap,
+      child: Container(
+        decoration: BoxDecoration(
+          borderRadius: const BorderRadius.all(Radius.circular(CosmosAppTheme.radiusS)),
+          border: Border.all(
+              color: lightInactive.withOpacity(word.isEmpty ? lightInactive.opacity / 2 : lightInactive.opacity)),
+          color: lightDivider.withOpacity(word.isEmpty ? lightDivider.opacity / 2 : lightDivider.opacity),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.symmetric(
+            horizontal: CosmosAppTheme.spacingS,
+            vertical: CosmosAppTheme.spacingXS,
+          ),
+          child: Row(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              if (showIndex) ...[
+                Text(
+                  "$index",
+                  style: textTheme.bodyText1,
+                ),
+                const SizedBox(width: CosmosAppTheme.spacingS),
+              ],
+              Text(
+                word,
+                style: textTheme.bodyText1?.copyWith(fontWeight: FontWeight.w700),
+              ),
+            ],
+          ),
         ),
       ),
-      labelStyle: const TextStyle(
-        color: Colors.white,
-        fontWeight: FontWeight.bold,
-      ),
-      selectedColor: Theme.of(context).primaryColorDark,
     );
   }
 }
--- packages/cosmos_ui_components/lib/components/mnemonic_words_grid.dart
+++ packages/cosmos_ui_components/lib/components/mnemonic_words_grid.dart
@@ -1,26 +1,42 @@
 import 'package:cosmos_ui_components/components/mnemonic_choice_chip.dart';
+import 'package:cosmos_ui_components/cosmos_ui_components.dart';
 import 'package:cosmos_utils/cosmos_utils.dart';
 import 'package:flutter/material.dart';
 
 class CosmosMnemonicWordsGrid extends StatelessWidget {
   final List<String> mnemonicWords;
   final ScrollPhysics? physics;
+  final bool showIndices;
+  final void Function(int index)? onTapWord;
 
   const CosmosMnemonicWordsGrid({
     Key? key,
     required this.mnemonicWords,
+    this.showIndices = true,
     this.physics,
+    this.onTapWord,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return GridView.count(
+    return GridView.extent(
       physics: physics,
-      crossAxisCount: (MediaQuery.of(context).size.width * 0.005).ceil(),
+      maxCrossAxisExtent: 150,
       shrinkWrap: true,
-      childAspectRatio: 5,
+      childAspectRatio: 3,
       children: mnemonicWords //
-          .mapIndexed((index, word) => MnemonicChoiceChip(index: index, word: word))
+          .mapIndexed((index, word) => Padding(
+                padding: const EdgeInsets.only(
+                  top: CosmosAppTheme.spacingS,
+                  left: CosmosAppTheme.spacingS,
+                ),
+                child: MnemonicChoiceChip(
+                  index: index,
+                  word: word,
+                  showIndex: showIndices,
+                  onTap: onTapWord == null ? null : () => onTapWord!.call(index),
+                ),
+              ))
           .toList(),
     );
   }
