fba8a8cbdbee1c9ee67d00a4d265d9c7806a62ca
Andrzej Chmielewski <andrzejchm@users.noreply.github.com>
2021-10-15 11:54:32 +0200

    bech32 address to hex converter (#105)

---
+++ packages/cosmos_utils/test/address_hexing_test.dart
@@ -0,0 +1,11 @@
+import 'package:cosmos_utils/cosmos_utils.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  test('bech32 to hex conversion', () {
+    const address = "cosmos1ygptsc84j7lezp6cvrjffu52520tla508qp4vk";
+    const expected = "2202b860f597bf91075860e494f28aa29ebff68f";
+
+    expect(bech32ToHex(address), expected);
+  });
+}
