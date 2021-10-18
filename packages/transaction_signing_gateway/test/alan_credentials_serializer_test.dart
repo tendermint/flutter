3a81bd30f04489ef1eb65944d8da94c79cf63860
Andrzej Chmielewski <andrzejchm@gmail.com>
2021-10-18 16:15:34 +0200

    fix tests

--- packages/transaction_signing_gateway/test/alan_credentials_serializer_test.dart
+++ packages/transaction_signing_gateway/test/alan_credentials_serializer_test.dart
@@ -1,4 +1,3 @@
-import 'package:alan/alan.dart' as alan;
 import 'package:flutter_test/flutter_test.dart';
 import 'package:transaction_signing_gateway/alan/alan_credentials_serializer.dart';
 import 'package:transaction_signing_gateway/alan/alan_private_wallet_credentials.dart';
@@ -7,14 +6,9 @@ import 'package:transaction_signing_gateway/model/wallet_public_info.dart';
 void main() {
   group("Alan Serializer tests", () {
     final serializer = AlanCredentialsSerializer();
-    final credentials = AlanPrivateWalletCredentials(
+    const credentials = AlanPrivateWalletCredentials(
       mnemonic: "mnemonic",
-      networkInfo: alan.NetworkInfo(
-        bech32Hrp: "cosmos",
-        lcdInfo: alan.LCDInfo(host: "localhost", port: 9919),
-        grpcInfo: alan.GRPCInfo(host: "localhost", port: 1919),
-      ),
-      publicInfo: const WalletPublicInfo(
+      publicInfo: WalletPublicInfo(
         chainId: "chainId",
         walletId: "walletId",
         name: 'name',
@@ -29,8 +23,6 @@ void main() {
       expect(credentialsResult.isRight(), true);
       final alanCredsResult = credentialsResult.getOrElse(() => throw "") as AlanPrivateWalletCredentials;
       expect(alanCredsResult, credentials);
-      expect(alanCredsResult.networkInfo.grpcInfo.port, 1919);
-      expect(alanCredsResult.networkInfo.lcdInfo.port, 9919);
     });
   });
 }
