eafb3b3e7b26dbf8a3b63a00b4e9d617787868f1
wal33d006 <waleed_arshad006@hotmail.com>
2021-11-01 22:42:02 +0500

    Fix the `formatAmount` function with the in-built `compactCurrency` function.

--- packages/cosmos_utils/lib/amount_formatter.dart
+++ packages/cosmos_utils/lib/amount_formatter.dart
@@ -1,22 +1,3 @@
 import 'package:intl/intl.dart';
 
-String formatAmount(double amount) {
-  final formatted = NumberFormat.currency(symbol: '\$').format(amount);
-  final raw = formatted.split('.')[0].substring(1).replaceAll(',', '');
-  if (raw.length >= 7) {
-    if (raw.length >= 16) {
-      return _getShortAmount(raw, 15, 'Q'); // Quadrillion
-    }
-    if (raw.length >= 13) {
-      return _getShortAmount(raw, 12, 'T'); // Trillion
-    }
-    if (raw.length >= 10) {
-      return _getShortAmount(raw, 9, 'B'); // Billion
-    }
-    return _getShortAmount(raw, 6, 'M'); // Million
-  }
-  return formatted;
-}
-
-String _getShortAmount(String raw, int numberOfZeros, String annotation) =>
-    '\$${raw.replaceRange(raw.length - numberOfZeros, raw.length, annotation)}';
+String formatAmount(double amount) => NumberFormat.compactCurrency(symbol: '\$').format(amount);
--- packages/cosmos_utils/test/number_formatter_test.dart
+++ packages/cosmos_utils/test/number_formatter_test.dart
@@ -3,6 +3,6 @@ import 'package:flutter_test/flutter_test.dart';
 
 void main() {
   test('Amount formatter test case', () {
-    expect(formatAmount(37570000000), '\$37B');
+    expect(formatAmount(37570000000), '\$37.6B');
   });
 }
