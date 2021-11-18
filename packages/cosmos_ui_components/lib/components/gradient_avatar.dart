ad388a651334c0b5eeb1bee9b0350cbf7fa37caa
waleedarshad <waleed_arshad006@hotmail.com>
2021-11-18 18:09:10 +0500

    Update gradient_avatar.dart

--- packages/cosmos_ui_components/lib/components/gradient_avatar.dart
+++ packages/cosmos_ui_components/lib/components/gradient_avatar.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:string_to_hex/string_to_hex.dart';
+import 'package:cosmos_utils/cosmos_utils.dart';
 
 class GradientAvatar extends StatelessWidget {
   final String stringKey;
@@ -17,25 +17,5 @@ class GradientAvatar extends StatelessWidget {
         ),
       ),
     );
-  }
-
-  List<Color> generateColorsFromAddress(String address) {
-    final color1 = StringBuffer();
-    final color2 = StringBuffer();
-
-    color1.write('#');
-    color2.write('#');
-
-    // Initialised it here because linter was warning me not to declare multiple variables in single line
-    var j = address.length - 7;
-    for (var i = address.length - 1; color1.length <= 7 && color2.length <= 7; i--, j--) {
-      color1.write(address[i]);
-      color2.write(address[j]);
-    }
-
-    return [
-      Color(StringToHex.toColor(color1.toString())),
-      Color(StringToHex.toColor(color2.toString())),
-    ];
   }
 }
