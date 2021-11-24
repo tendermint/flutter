dbf8f782d2804661f4d07f186d84fa31c427e88e
Andrzej Chmielewski <andrzejchm@users.noreply.github.com>
2021-11-24 08:05:22 +0100

    feat: Backup wallet screen - intro (#127)

--- starport_template/lib/starport_app.dart
+++ starport_template/lib/starport_app.dart
@@ -5,12 +5,14 @@ import 'package:starport_template/pages/routing_page.dart';
 import 'package:starport_template/stores/wallets_store.dart';
 import 'package:starport_template/utils/base_env.dart';
 import 'package:transaction_signing_gateway/gateway/transaction_signing_gateway.dart';
+import 'package:transaction_signing_gateway/storage/biometric_data_store.dart';
 
 class StarportApp extends StatelessWidget {
   static late TransactionSigningGateway signingGateway;
   static late WalletsStore walletsStore;
   static late BaseEnv baseEnv;
   static late NetworkInfo networkInfo;
+  static late BiometricDataStore biometricDataStore;
 
   @override
   Widget build(BuildContext context) {
---
+++ starport_template/lib/widgets/loading_splash.dart
@@ -0,0 +1,35 @@
+import 'package:cosmos_ui_components/cosmos_text_theme.dart';
+import 'package:cosmos_ui_components/cosmos_theme.dart';
+import 'package:flutter/material.dart';
+
+class LoadingSplash extends StatelessWidget {
+  final String text;
+
+  const LoadingSplash({
+    Key? key,
+    required this.text,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      child: Padding(
+        padding: EdgeInsets.symmetric(horizontal: CosmosTheme.of(context).spacingM),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            CircularProgressIndicator(
+              color: CosmosTheme.of(context).colors.onBackground,
+            ),
+            SizedBox(height: CosmosTheme.of(context).spacingL),
+            Text(
+              text,
+              textAlign: TextAlign.center,
+              style: CosmosTextTheme.title2Bold,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
