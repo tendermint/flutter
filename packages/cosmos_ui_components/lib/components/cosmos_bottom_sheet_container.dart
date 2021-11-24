e93f7358e64ebcdd9f6245c8a0d5df23a593f2b8
waleedarshad <waleed_arshad006@hotmail.com>
2021-11-24 23:38:16 +0500

    Comment out show bottom sheet function

--- packages/cosmos_ui_components/lib/components/cosmos_bottom_sheet_container.dart
+++ packages/cosmos_ui_components/lib/components/cosmos_bottom_sheet_container.dart
@@ -27,20 +27,21 @@ class CosmosBottomSheetContainer extends StatelessWidget {
   }
 }
 
-Future showCosmosBottomSheet(Widget child, BuildContext context, {bool isFullScreen = true}) => showModalBottomSheet(
-      context: context,
-      isScrollControlled: true,
-      enableDrag: true,
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.vertical(
-          top: Radius.elliptical(
-            CosmosTheme.of(context).radiusL,
-            CosmosTheme.of(context).radiusL * 1.1,
-          ),
-        ),
-      ),
-      builder: (context) => SizedBox(
-        height: isFullScreen ? MediaQuery.of(context).size.height / 1.06 : MediaQuery.of(context).size.height / 2,
-        child: CosmosBottomSheetContainer(child: child),
-      ),
-    );
+// TODO: Commenting it for now but can be used if the spacing problem for `showMaterialModalBottomSheet` persists
+// Future showCosmosBottomSheet(Widget child, BuildContext context, {bool isFullScreen = true}) => showModalBottomSheet(
+//       context: context,
+//       isScrollControlled: true,
+//       enableDrag: true,
+//       shape: RoundedRectangleBorder(
+//         borderRadius: BorderRadius.vertical(
+//           top: Radius.elliptical(
+//             CosmosTheme.of(context).radiusL,
+//             CosmosTheme.of(context).radiusL * 1.1,
+//           ),
+//         ),
+//       ),
+//       builder: (context) => SizedBox(
+//         height: isFullScreen ? MediaQuery.of(context).size.height / 1.06 : MediaQuery.of(context).size.height / 2,
+//         child: CosmosBottomSheetContainer(child: child),
+//       ),
+//     );
