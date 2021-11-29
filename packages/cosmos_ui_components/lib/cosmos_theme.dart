3658a914f1099e36c49d4d443c201669b9cd25f2
Andrzej Chmielewski <andrzejchm@gmail.com>
2021-11-29 13:05:33 +0100

    fix cosmos_theme

--- packages/cosmos_ui_components/lib/cosmos_theme.dart
+++ packages/cosmos_ui_components/lib/cosmos_theme.dart
@@ -139,8 +139,8 @@ class CosmosColorsData extends Equatable {
   static const darkSurface = Color(0xFF171717);
   static const onDarkText = Color(0xFFFFFFFF);
   static const darkDivider = Color(0x17FFFFFF);
-
   static const error = Color(0xFFE74444);
+  
   final Color inactive;
   final Color divider;
   final Color text;
@@ -166,5 +166,16 @@ class CosmosColorsData extends Equatable {
   });
 
   @override
-  List<Object?> get props => [];
+  List<Object?> get props => [
+    inactive,
+    divider,
+    text,
+    background,
+    cardBackground,
+    actionSheetPositive,
+    actionSheetDestructive,
+    chipBackground,
+    link,
+    positiveText,
+  ];
 }
