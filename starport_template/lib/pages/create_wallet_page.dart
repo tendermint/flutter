1b93fa3a43487cbb101834a64a4d4ec6ffc8097d
Andrzej Chmielewski <andrzejchm@gmail.com>
2021-11-30 12:58:19 +0100

    fix static code analysis

--- starport_template/lib/pages/create_wallet_page.dart
+++ starport_template/lib/pages/create_wallet_page.dart
@@ -72,7 +72,7 @@ class _CreateWalletPageState extends State<CreateWalletPage> {
 
   CosmosAppBar _appBar() {
     return CosmosAppBar(
-      leading: const CosmosBackButton(text: "Back"),
+      leading: const CosmosBackButton(),
       title: "Back up your account",
       actions: [
         CosmosAppBarAction(
