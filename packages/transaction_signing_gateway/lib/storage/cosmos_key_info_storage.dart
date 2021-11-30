e9c7c0e5c19830dad6a7082011a3547f47528d1b
Waleed Arshad <25613623+wal33d006@users.noreply.github.com>
2021-11-30 16:47:17 +0500

    feat: Add function for updating public wallet info using plain data store (#138)
    * Add function for updating public wallet info using plain data store
    
    * Add error logging in catch clause
    
    * Review PR suggestions

--- packages/transaction_signing_gateway/lib/storage/cosmos_key_info_storage.dart
+++ packages/transaction_signing_gateway/lib/storage/cosmos_key_info_storage.dart
@@ -167,4 +167,26 @@ class CosmosKeyInfoStorage implements KeyInfoStorage {
     final privateCreds = await getPrivateCredentials(walletLookupKey);
     return right(privateCreds.isRight());
   }
+
+  @override
+  Future<Either<CredentialsStorageFailure, Unit>> updatePublicWalletInfo({required WalletPublicInfo info}) async {
+    try {
+      final publicInfoKey = _publicInfoKey(
+        chainId: info.chainId,
+        walletId: info.walletId,
+      );
+
+      return _plainDataStore.readPlainText(key: publicInfoKey).flatMap((walletInfo) async {
+        if (walletInfo == null) {
+          return left(const CredentialsStorageFailure("Wallet not found"));
+        }
+
+        final publicInfoJson = await compute(jsonEncode, WalletPublicInfoSerializer.toMap(info));
+        return _plainDataStore.savePlainText(key: publicInfoKey, value: publicInfoJson);
+      });
+    } catch (e, stack) {
+      logError(e, stack);
+      return left(CredentialsStorageFailure("$e"));
+    }
+  }
 }
