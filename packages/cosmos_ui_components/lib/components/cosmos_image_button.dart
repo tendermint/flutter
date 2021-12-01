4e119a25a853c684f1fe000cf26d71e33784ec91
Waleed Arshad <25613623+wal33d006@users.noreply.github.com>
2021-12-01 18:50:23 +0500

    feat: Rename wallet - Components (#141)
    * Rename account - Components
    
    * Update pubspec.lock
    
    * Add clear button background color to cosmos_theme.dart
    
    * Add cosmos_text_field.dart.
    
    * Update cosmos_ui_components.dart
    
    * Review PR suggestions
    
    * Review PR suggestions
    
    * Remove required from onTap parameter in cosmos_image_button.dart

---
+++ packages/cosmos_ui_components/lib/components/cosmos_image_button.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class CosmosImageButton extends StatelessWidget {
+  final String asset;
+  final double height;
+  final double width;
+  final VoidCallback? onTap;
+
+  const CosmosImageButton({
+    Key? key,
+    required this.asset,
+    this.onTap,
+    this.height = 24,
+    this.width = 24,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: onTap,
+      child: SizedBox(
+        height: height,
+        width: width,
+        child: Image.asset(asset),
+      ),
+    );
+  }
+}
