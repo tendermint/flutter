758fbcdf62059cc92f26ccbd5fa0f75e0755b029
waleedarshad <waleed_arshad006@hotmail.com>
2021-12-03 10:39:31 +0500

    Refactor UI

--- starport_template/lib/pages/transfer_asset_page.dart
+++ starport_template/lib/pages/transfer_asset_page.dart
@@ -1,9 +1,10 @@
+import 'package:cosmos_ui_components/components/template/cosmos_balance_card.dart';
 import 'package:cosmos_ui_components/cosmos_ui_components.dart';
 import 'package:flutter/material.dart';
 import 'package:starport_template/entities/balance.dart';
 import 'package:starport_template/entities/msg_send_transaction.dart';
 import 'package:starport_template/pages/sign_transaction_page.dart';
-import 'package:starport_template/widgets/balance_card_list.dart';
+import 'package:starport_template/widgets/send_money_form.dart';
 
 class TransferAssetPage extends StatefulWidget {
   final Balance balance;
@@ -38,56 +39,23 @@ class _TransferAssetPageState extends State<TransferAssetPage> {
             isListTileType: true,
           ),
           SizedBox(height: theme.spacingXXL),
-          _buildTextFields(theme, context),
-          const Spacer(),
-          _buildFooterButton(theme),
-        ],
-      ),
-    );
-  }
-
-  void _navigateToSignTransaction() {
-    Navigator.of(context).push(
-      MaterialPageRoute(
-        builder: (context) => SignTransactionPage(
-          transaction: MsgSendTransaction(amount: amount, fee: fee, recipient: walletAddress),
-          balance: widget.balance,
-        ),
-      ),
-    );
-  }
-
-  Padding _buildTextFields(CosmosThemeData theme, BuildContext context) {
-    return Padding(
-      padding: EdgeInsets.symmetric(horizontal: theme.spacingL),
-      child: Column(
-        children: [
-          CosmosTextField(
-            onChanged: (value) {
+          SendMoneyForm(
+            onAddressChanged: (value) {
               walletAddress = value;
               setState(() {});
             },
-            hint: "Enter receiver's wallet address",
-            suffix: CosmosTextButton(onTap: () {}, text: 'Paste', color: CosmosTheme.of(context).colors.link),
-          ),
-          SizedBox(height: theme.spacingL),
-          CosmosTextField(
-            onChanged: (value) {
+            onAmountChanged: (value) {
               amount = _validateAmount(value);
               setState(() {});
             },
-            keyboardType: TextInputType.number,
-            hint: '0 ${widget.balance.denom.text.toUpperCase()}',
-          ),
-          SizedBox(height: theme.spacingL),
-          CosmosTextField(
-            onChanged: (value) {
+            onFeeChanged: (value) {
               fee = _validateAmount(value);
               setState(() {});
             },
-            keyboardType: TextInputType.number,
-            hint: 'Transaction fee',
+            denomText: widget.balance.denom.text,
           ),
+          const Spacer(),
+          _footerButton(theme),
         ],
       ),
     );
@@ -105,7 +73,7 @@ class _TransferAssetPageState extends State<TransferAssetPage> {
     }
   }
 
-  SafeArea _buildFooterButton(CosmosThemeData theme) {
+  SafeArea _footerButton(CosmosThemeData theme) {
     return SafeArea(
       child: Row(
         children: [
@@ -114,11 +82,22 @@ class _TransferAssetPageState extends State<TransferAssetPage> {
               padding: EdgeInsets.symmetric(horizontal: theme.spacingL),
               child: CosmosElevatedButton(
                 text: 'Continue',
-                onTap: isTransferValidated ? _navigateToSignTransaction : null,
+                onTap: isTransferValidated ? _onTapContinue : null,
               ),
             ),
           ),
         ],
+      ),
+    );
+  }
+
+  void _onTapContinue() {
+    Navigator.of(context).push(
+      MaterialPageRoute(
+        builder: (context) => SignTransactionPage(
+          transaction: MsgSendTransaction(amount: amount, fee: fee, recipient: walletAddress),
+          balance: widget.balance,
+        ),
       ),
     );
   }
--- starport_template/lib/widgets/balance_card_list.dart
+++ starport_template/lib/widgets/balance_card_list.dart
@@ -1,7 +1,4 @@
-// import 'package:cosmos_ui_components/components/template/cosmos_balance_card.dart';
-import 'package:cosmos_ui_components/components/cosmos_token_avatar.dart';
 import 'package:cosmos_ui_components/components/template/cosmos_balance_card.dart';
-import 'package:cosmos_ui_components/cosmos_text_theme.dart';
 import 'package:cosmos_ui_components/cosmos_theme.dart';
 import 'package:cosmos_utils/amount_formatter.dart';
 import 'package:flutter/material.dart';
@@ -32,74 +29,6 @@ class BalanceCardList extends StatelessWidget {
               ),
             )
             .toList(),
-      ),
-    );
-  }
-}
-
-class CosmosBalanceCard extends StatelessWidget {
-  final String denomText;
-  final String amountDisplayText;
-  final VoidCallback? onTap;
-  final bool isListTileType;
-
-  const CosmosBalanceCard({
-    required this.denomText,
-    required this.amountDisplayText,
-    this.isListTileType = false,
-    this.onTap,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return InkWell(
-      onTap: onTap,
-      child: Padding(
-        padding: EdgeInsets.symmetric(horizontal: CosmosTheme.of(context).spacingL),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Column(
-              children: [
-                Row(
-                  children: [
-                    CosmosTokenAvatar(text: denomText),
-                    SizedBox(width: CosmosTheme.of(context).spacingM),
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          denomText,
-                          style: CosmosTextTheme.title0Medium,
-                        ),
-                        if (isListTileType)
-                          Text(
-                            '$amountDisplayText ${denomText.toUpperCase()}',
-                            style: CosmosTextTheme.copyMinus1Normal,
-                          ),
-                      ],
-                    ),
-                  ],
-                ),
-              ],
-            ),
-            if (!isListTileType)
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.end,
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text(
-                    amountDisplayText,
-                    style: CosmosTextTheme.labelS,
-                  ),
-                  Text(
-                    'available ${denomText.toUpperCase()}',
-                    style: CosmosTextTheme.copyMinus1Normal,
-                  ),
-                ],
-              ),
-          ],
-        ),
       ),
     );
   }
