974b5dd165a4516b9d34ab593054903b950095f0
Andrzej Chmielewski <andrzejchm@gmail.com>
2021-12-09 11:06:50 +0100

    post-rebase fixes

--- packages/cosmos_utils/test/mnemonic_test.dart
+++ packages/cosmos_utils/test/mnemonic_test.dart
@@ -8,14 +8,14 @@ void main() {
     expect(validateMnemonic(""), MnemonicValidationError.MnemonicEmpty);
     expect(
       validateMnemonic(
-          "pink later snow hire pill message improve jeans supply reflect wave right silent inflict "
+        "pink later snow hire pill message improve jeans supply reflect wave right silent inflict "
         "fiction riot hurdle belt awkward zero direct will giant dose",
       ),
       MnemonicValidationError.Unknown,
     );
     expect(
       validateMnemonic(
-          "later pink snow hire pill message improve jeans supply reflect "
+        "later pink snow hire pill message improve jeans supply reflect "
         "wave right silent inflict fiction riot hurdle belt awkward zero direct will giant dose",
       ),
       null,
