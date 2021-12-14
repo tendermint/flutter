3edd89831bbb8fae46722792681ebed280ff63fb
waleedarshad <waleed_arshad006@hotmail.com>
2021-12-14 12:20:22 +0500

    Remove card widget

--- starport_template/lib/widgets/transaction_history_list.dart
+++ starport_template/lib/widgets/transaction_history_list.dart
@@ -1,3 +1,4 @@
+import 'package:cosmos_ui_components/components/template/transaction_history_card.dart';
 import 'package:cosmos_ui_components/components/template/transaction_history_group_title.dart';
 import 'package:cosmos_ui_components/cosmos_text_theme.dart';
 import 'package:cosmos_ui_components/cosmos_ui_components.dart';
@@ -51,76 +52,6 @@ class _TransactionHistoryListState extends State<TransactionHistoryList> {
           );
         },
       ).toList(),
-    );
-  }
-}
-
-class TransactionHistoryCard extends StatelessWidget {
-  const TransactionHistoryCard({
-    Key? key,
-    required this.isOutgoing,
-    required this.transactionType,
-    required this.date,
-    required this.amountText,
-    required this.denomText,
-  }) : super(key: key);
-
-  final bool isOutgoing;
-  final String transactionType;
-  final DateTime date;
-  final String amountText;
-  final String denomText;
-
-  @override
-  Widget build(BuildContext context) {
-    final theme = CosmosTheme.of(context);
-    return Padding(
-      padding: EdgeInsets.symmetric(horizontal: theme.spacingL, vertical: theme.spacingM),
-      child: Column(
-        children: [
-          Row(
-            children: [
-              RotatedBox(
-                quarterTurns: isOutgoing ? 2 : 0,
-                child: Image.asset(
-                  'assets/images/receive_icon.png',
-                  package: packageName,
-                ),
-              ),
-              SizedBox(width: theme.spacingL),
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    transactionType,
-                    style: CosmosTextTheme.labelS,
-                  ),
-                  SizedBox(height: theme.spacingXS),
-                  Text(
-                    formatDate(date, DateFormatEnum.ShortUIDateDay),
-                    style: CosmosTextTheme.copyMinus1Normal,
-                  ),
-                ],
-              ),
-              const Spacer(),
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.end,
-                children: [
-                  Text(
-                    '${isOutgoing ? '-' : '+'} $amountText',
-                    style: CosmosTextTheme.labelS.copyWith(color: isOutgoing ? null : Colors.green),
-                  ),
-                  SizedBox(height: theme.spacingXS),
-                  Text(
-                    denomText.toUpperCase(),
-                    style: CosmosTextTheme.copyMinus1Normal,
-                  )
-                ],
-              )
-            ],
-          ),
-        ],
-      ),
     );
   }
 }
