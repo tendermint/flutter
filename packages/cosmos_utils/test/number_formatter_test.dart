0e01d5e2127ef2352ed5463976a3b30df2baa1b2
Andrzej Chmielewski <andrzejchm@users.noreply.github.com>
2021-12-14 18:25:41 +0100

    refactor: Flutter & Linter update - cosmos_utils (#163)

--- packages/cosmos_utils/test/number_formatter_test.dart
+++ packages/cosmos_utils/test/number_formatter_test.dart
@@ -3,11 +3,11 @@ import 'package:flutter_test/flutter_test.dart';
 
 void main() {
   test('Amount formatter test case', () {
-    expect(formatAmount(37570000000), '\$37.6B');
-    expect(formatAmount(10270, locale: "pl", symbol: "PLN"), '10,3 tys. PLN');
-    expect(formatAmount(1027.004921), '\$1,027.00');
-    expect(formatAmount(10000), '\$10K');
-    expect(formatAmount(9999), '\$9,999.00');
-    expect(formatAmount(1027), '\$1,027.00');
+    expect(formatAmount(37570000000), r'$37.6B');
+    expect(formatAmount(10270, locale: 'pl', symbol: 'PLN'), '10,3 tys. PLN');
+    expect(formatAmount(1027.004921), r'$1,027.00');
+    expect(formatAmount(10000), r'$10K');
+    expect(formatAmount(9999), r'$9,999.00');
+    expect(formatAmount(1027), r'$1,027.00');
   });
 }
