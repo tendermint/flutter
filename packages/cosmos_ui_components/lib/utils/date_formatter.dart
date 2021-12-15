cfb07683372ae7b2de8a4a04b7b4e61dc846cdea
waleedarshad <waleed_arshad006@hotmail.com>
2021-12-15 15:40:35 +0500

    Update date_formatter.dart

--- packages/cosmos_ui_components/lib/utils/date_formatter.dart
+++ packages/cosmos_ui_components/lib/utils/date_formatter.dart
@@ -2,6 +2,8 @@ import 'package:intl/intl.dart';
 
 String formatDate(DateTime date, DateFormatEnum dateFormat) => DateFormat(getDateFormat[dateFormat]).format(date);
 
+String getMonthFromGroupedKey(String groupedMonthYear) => groupedMonthYear.split('-')[0];
+
 abstract class DateFormats {
   static const String uiShortDate = 'MMM d';
   static const String groupByMonthYear = 'MMMM-yyyy';
