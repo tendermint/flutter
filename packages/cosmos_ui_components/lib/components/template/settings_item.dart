995fe0c2ffcda76ffb8ccab2525b9addeb6f6988
waleedarshad <waleed_arshad006@hotmail.com>
2021-12-17 17:24:54 +0500

    Add settings_item.dart

---
+++ packages/cosmos_ui_components/lib/components/template/settings_item.dart
@@ -0,0 +1,46 @@
+import 'package:cosmos_ui_components/cosmos_ui_components.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+
+class SettingsItem extends StatelessWidget {
+  const SettingsItem({
+    required this.text,
+    required this.textStyle,
+    this.infoIcon,
+    this.onTap,
+    Key? key,
+  }) : super(key: key);
+
+  final String text;
+  final TextStyle textStyle;
+  final Widget? infoIcon;
+  final VoidCallback? onTap;
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = CosmosTheme.of(context);
+    return Padding(
+      padding: EdgeInsets.symmetric(horizontal: theme.spacingL, vertical: theme.spacingM),
+      child: InkWell(
+        onTap: onTap,
+        child: Row(
+          children: [
+            Text(text, style: textStyle),
+            const Spacer(),
+            if (infoIcon != null) infoIcon!,
+            Image.asset('assets/images/arrow_right.png'),
+          ],
+        ),
+      ),
+    );
+  }
+
+  @override
+  void debugFillProperties(DiagnosticPropertiesBuilder properties) {
+    super.debugFillProperties(properties);
+    properties
+      ..add(StringProperty('text', text))
+      ..add(DiagnosticsProperty<TextStyle>('textStyle', textStyle))
+      ..add(ObjectFlagProperty<VoidCallback?>.has('onTap', onTap));
+  }
+}
