2cac26b5c01a8f290b73a4dff00f6b420d2ac3a9
waleedarshad <waleed_arshad006@hotmail.com>
2021-12-17 23:11:02 +0500

    Review PR suggestions

--- starport_template/lib/widgets/settings_sheet.dart
+++ starport_template/lib/widgets/settings_sheet.dart
@@ -1,7 +1,7 @@
+import 'package:cosmos_ui_components/components/template/settings_item.dart';
 import 'package:cosmos_ui_components/cosmos_text_theme.dart';
 import 'package:cosmos_ui_components/cosmos_ui_components.dart';
 import 'package:cosmos_utils/cosmos_utils.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 class SettingsSheet extends StatelessWidget {
@@ -10,6 +10,7 @@ class SettingsSheet extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final theme = CosmosTheme.of(context);
+    final textStyle = CosmosTextTheme.copy0Normal.copyWith(color: CosmosTheme.of(context).colors.link);
     return CosmosBottomSheetContainer(
       child: Column(
         children: [
@@ -39,17 +40,17 @@ class SettingsSheet extends StatelessWidget {
           SizedBox(height: theme.spacingXL),
           SettingsItem(
             text: 'Join the Telegram community',
-            textStyle: CosmosTextTheme.copy0Normal.copyWith(color: CosmosTheme.of(context).colors.link),
+            textStyle: textStyle,
             onTap: () => notImplemented(context),
           ),
           SettingsItem(
             text: 'Twitter',
-            textStyle: CosmosTextTheme.copy0Normal.copyWith(color: CosmosTheme.of(context).colors.link),
+            textStyle: textStyle,
             onTap: () => notImplemented(context),
           ),
           SettingsItem(
             text: 'Starport',
-            textStyle: CosmosTextTheme.copy0Normal.copyWith(color: CosmosTheme.of(context).colors.link),
+            textStyle: textStyle,
             onTap: () => notImplemented(context),
           ),
           const Spacer(),
@@ -62,48 +63,5 @@ class SettingsSheet extends StatelessWidget {
         ],
       ),
     );
-  }
-}
-
-class SettingsItem extends StatelessWidget {
-  const SettingsItem({
-    required this.text,
-    required this.textStyle,
-    this.infoIcon,
-    this.onTap,
-    Key? key,
-  }) : super(key: key);
-
-  final String text;
-  final TextStyle textStyle;
-  final Widget? infoIcon;
-  final VoidCallback? onTap;
-
-  @override
-  Widget build(BuildContext context) {
-    final theme = CosmosTheme.of(context);
-    return Padding(
-      padding: EdgeInsets.symmetric(horizontal: theme.spacingL, vertical: theme.spacingM),
-      child: InkWell(
-        onTap: onTap,
-        child: Row(
-          children: [
-            Text(text, style: textStyle),
-            const Spacer(),
-            if (infoIcon != null) infoIcon!,
-            Image.asset('assets/images/arrow_right.png'),
-          ],
-        ),
-      ),
-    );
-  }
-
-  @override
-  void debugFillProperties(DiagnosticPropertiesBuilder properties) {
-    super.debugFillProperties(properties);
-    properties
-      ..add(StringProperty('text', text))
-      ..add(DiagnosticsProperty<TextStyle>('textStyle', textStyle))
-      ..add(ObjectFlagProperty<VoidCallback?>.has('onTap', onTap));
   }
 }
