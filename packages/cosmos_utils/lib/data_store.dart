ec8ce792f2f01b3b8de75b252dca7c2708d0e6fa
Andrzej Chmielewski <andrzejchm@users.noreply.github.com>
2022-01-18 13:33:58 +0100

    biometrics refactor - part 1 - cosmos_utils (#191)

---
+++ packages/cosmos_utils/lib/data_store.dart
@@ -0,0 +1,26 @@
+import 'package:cosmos_utils/credentials_storage_failure.dart';
+import 'package:dartz/dartz.dart';
+
+abstract class PlainDataStore {
+  Future<Either<CredentialsStorageFailure, Unit>> savePlainText({
+    required String key,
+    required String? value,
+  });
+
+  Future<Either<CredentialsStorageFailure, String?>> readPlainText({
+    required String key,
+  });
+
+  Future<Either<CredentialsStorageFailure, Map<String, String?>>> readAllPlainText();
+}
+
+abstract class SecureDataStore {
+  Future<Either<CredentialsStorageFailure, Unit>> saveSecureText({
+    required String key,
+    required String? value,
+  });
+
+  Future<Either<CredentialsStorageFailure, String?>> readSecureText({
+    required String key,
+  });
+}
