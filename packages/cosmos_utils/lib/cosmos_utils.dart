7e7e433682ecb91adfe0215515c53a52a36205ae
Andrzej Chmielewski <andrzejchm@gmail.com>
2022-01-18 13:28:46 +0100

    biometrics refactor - part 1 - cosmos_utils

--- packages/cosmos_utils/lib/cosmos_utils.dart
+++ packages/cosmos_utils/lib/cosmos_utils.dart
@@ -3,6 +3,8 @@ library cosmos_utils;
 export 'address_parser.dart';
 export 'app_info_provider.dart';
 export 'colors_generator.dart';
+export 'credentials_storage_failure.dart';
+export 'data_store.dart';
 export 'extensions.dart';
 export 'future_either.dart';
 export 'group_by_extension.dart';
