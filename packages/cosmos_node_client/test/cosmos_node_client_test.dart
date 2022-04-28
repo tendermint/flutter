a5800a352355e31303318110631939fdcb90c836
Chiziaruhoma <chiziaruhoma@gmail.com>
2022-04-28 12:46:30 +0100

    updated test and added cosmoslint

--- packages/cosmos_node_client/test/cosmos_node_client_test.dart
+++ packages/cosmos_node_client/test/cosmos_node_client_test.dart
@@ -1,1 +1,10 @@
-void main() {}
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  test('demo test to be removed', () async {
+    const two = 2;
+    const three = 3;
+
+    expect(two * three, equals(6));
+  });
+}
