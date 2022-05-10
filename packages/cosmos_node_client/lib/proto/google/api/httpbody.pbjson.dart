d81001187a9a4cbaf54bc2460a2c997c8a520c38
Chiziaruhoma <chiziaruhoma@gmail.com>
2022-05-10 12:09:29 +0100

    generated proto files

---
+++ packages/cosmos_node_client/lib/proto/google/api/httpbody.pbjson.dart
@@ -0,0 +1,22 @@
+///
+//  Generated code. Do not modify.
+//  source: google/api/httpbody.proto
+//
+// @dart = 2.12
+// ignore_for_file: annotate_overrides,camel_case_types,unnecessary_const,non_constant_identifier_names,library_prefixes,unused_import,unused_shown_name,return_of_invalid_type,unnecessary_this,prefer_final_fields,deprecated_member_use_from_same_package
+
+import 'dart:core' as $core;
+import 'dart:convert' as $convert;
+import 'dart:typed_data' as $typed_data;
+@$core.Deprecated('Use httpBodyDescriptor instead')
+const HttpBody$json = const {
+  '1': 'HttpBody',
+  '2': const [
+    const {'1': 'content_type', '3': 1, '4': 1, '5': 9, '10': 'contentType'},
+    const {'1': 'data', '3': 2, '4': 1, '5': 12, '10': 'data'},
+    const {'1': 'extensions', '3': 3, '4': 3, '5': 11, '6': '.google.protobuf.Any', '10': 'extensions'},
+  ],
+};
+
+/// Descriptor for `HttpBody`. Decode as a `google.protobuf.DescriptorProto`.
+final $typed_data.Uint8List httpBodyDescriptor = $convert.base64Decode('CghIdHRwQm9keRIhCgxjb250ZW50X3R5cGUYASABKAlSC2NvbnRlbnRUeXBlEhIKBGRhdGEYAiABKAxSBGRhdGESNAoKZXh0ZW5zaW9ucxgDIAMoCzIULmdvb2dsZS5wcm90b2J1Zi5BbnlSCmV4dGVuc2lvbnM=');
---
+++ packages/cosmos_node_client/scripts/generate_proto.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+set -eo pipefail
+
+# Define variables
+OUT=lib/proto
+PROTO=proto
+THIRD_PARTY=third_party
+COSMOS_VERSION=0.45.4
+
+# Generate the third party Protobuf implementations
+PROTOC="protoc --dart_out=grpc:$OUT -I$THIRD_PARTY/proto"
+proto_dirs=$(find "$THIRD_PARTY/proto" -path -prune -o -name '*.proto' -print0 | xargs -0 -n1 dirname | sort | uniq)
+for dir in $proto_dirs; do
+  $PROTOC -I$THIRD_PARTY/proto $(find "${dir}" -maxdepth 1 -name '*.proto')
+done
