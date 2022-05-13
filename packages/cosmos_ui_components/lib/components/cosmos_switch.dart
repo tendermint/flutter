fbe4a4872db51b4ede9f987f293a1dea1ea127c3
waleedarshad <waleed_arshad006@hotmail.com>
2022-05-13 14:28:49 +0500

    Review PR suggestions

--- packages/cosmos_ui_components/lib/components/cosmos_switch.dart
+++ packages/cosmos_ui_components/lib/components/cosmos_switch.dart
@@ -1,101 +1,59 @@
 import 'package:cosmos_ui_components/cosmos_theme.dart';
-import 'package:cosmos_ui_components/utils/global_constants.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
-class CosmosSwitch extends StatefulWidget {
+class CosmosSwitch extends StatelessWidget {
   const CosmosSwitch({
     required this.checked,
+    required this.toggleAsset,
+    required this.checkedGradientStops,
+    required this.gradientColors,
     this.onChanged,
     Key? key,
   }) : super(key: key);
 
   final bool checked;
   final ValueChanged<bool>? onChanged;
+  final String toggleAsset;
+  final List<double> checkedGradientStops;
+  final List<Color> gradientColors;
 
   @override
-  CosmosSwitchState createState() => CosmosSwitchState();
-
-  @override
-  void debugFillProperties(DiagnosticPropertiesBuilder properties) {
-    super.debugFillProperties(properties);
-    properties
-      ..add(ObjectFlagProperty<ValueChanged<bool>?>.has('onChanged', onChanged))
-      ..add(DiagnosticsProperty<bool>('checked', checked));
-  }
-}
-
-class CosmosSwitchState extends State<CosmosSwitch> with SingleTickerProviderStateMixin {
-  late Animation<Alignment> _circleAnimation;
-  late AnimationController _animationController;
-
-  @override
-  void initState() {
-    super.initState();
-    _animationController = AnimationController(vsync: this, duration: const Duration(milliseconds: 200));
-    _circleAnimation = AlignmentTween(
-      begin: widget.checked ? Alignment.centerRight : Alignment.centerLeft,
-      end: widget.checked ? Alignment.centerLeft : Alignment.centerRight,
-    ).animate(
-      CurvedAnimation(
-        parent: _animationController,
-        curve: Curves.linear,
+  Widget build(BuildContext context) {
+    return TweenAnimationBuilder<Alignment>(
+      duration: const Duration(milliseconds: 200),
+      tween: AlignmentTween(
+        begin: checked ? Alignment.centerLeft : Alignment.centerRight,
+        end: checked ? Alignment.centerRight : Alignment.centerLeft,
       ),
-    );
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: () {
-        if (_animationController.isCompleted) {
-          _animationController.reverse();
-        } else {
-          _animationController.forward();
-        }
-        widget.onChanged!(!widget.checked);
-      },
-      child: _AnimatedSwitch(animation: _circleAnimation, checked: widget.checked),
-    );
-  }
-}
-
-class _AnimatedSwitch extends AnimatedWidget {
-  const _AnimatedSwitch({
-    required Animation<Alignment> animation,
-    required this.checked,
-    Key? key,
-  }) : super(key: key, listenable: animation);
-
-  final bool checked;
-
-  @override
-  Widget build(BuildContext context) {
-    final animation = listenable as Animation<Alignment>;
-    return Transform.scale(
-      scale: 0.9,
-      child: Container(
-        width: 50,
-        height: 28,
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(30),
-          gradient: checked
-              ? const LinearGradient(
-                  stops: [0, 0.5866, 1],
-                  colors: GlobalConstants.cosmosGradientColors,
-                )
-              : null,
-          color: checked ? null : CosmosTheme.of(context).colors.text,
-        ),
+      builder: (context, value, child) => GestureDetector(
+        onTap: () => onChanged!(!checked),
         child: Transform.scale(
-          scale: 1.1,
-          child: Align(
-            alignment: animation.value,
-            child: SizedBox(
-              width: 30,
-              height: 30,
-              child: Image.asset('assets/images/toggle.png'),
+          scale: 0.9,
+          child: Container(
+            width: 50,
+            height: 28,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(30),
+              gradient: checked
+                  ? LinearGradient(
+                      stops: checkedGradientStops,
+                      colors: gradientColors,
+                    )
+                  : null,
+              color: checked ? null : CosmosTheme.of(context).colors.text,
+            ),
+            child: Transform.scale(
+              scale: 1.1,
+              child: Align(
+                alignment: value,
+                child: SizedBox(
+                  width: 30,
+                  height: 30,
+                  child: Image.asset(toggleAsset),
+                ),
+              ),
             ),
           ),
         ),
@@ -106,6 +64,11 @@ class _AnimatedSwitch extends AnimatedWidget {
   @override
   void debugFillProperties(DiagnosticPropertiesBuilder properties) {
     super.debugFillProperties(properties);
-    properties.add(DiagnosticsProperty<bool>('checked', checked));
+    properties
+      ..add(DiagnosticsProperty<bool>('checked', checked))
+      ..add(ObjectFlagProperty<ValueChanged<bool>?>.has('onChanged', onChanged))
+      ..add(StringProperty('toggleAsset', toggleAsset))
+      ..add(IterableProperty<double>('checkedGradientStops', checkedGradientStops))
+      ..add(IterableProperty<Color>('gradientColors', gradientColors));
   }
 }
--- packages/cosmos_ui_components/lib/utils/global_constants.dart
+++ packages/cosmos_ui_components/lib/utils/global_constants.dart
@@ -1,11 +1,3 @@
-import 'package:flutter/material.dart';
-
 abstract class GlobalConstants {
   static const double defaultButtonHeight = 50;
-
-  static const List<Color> cosmosGradientColors = [
-    Color(0xFF64DBFC),
-    Color(0xFF30FFDF),
-    Color(0xFFFFFE39),
-  ];
 }
