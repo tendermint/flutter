ac470e1864a1728b563751020d9f454f28eecdc1
waleedarshad <waleed_arshad006@hotmail.com>
2022-05-19 15:07:56 +0500

    Introduct optional colors for cosmos_outline_button.dart

--- packages/cosmos_ui_components/lib/components/cosmos_outline_button.dart
+++ packages/cosmos_ui_components/lib/components/cosmos_outline_button.dart
@@ -1,3 +1,4 @@
+import 'package:cosmos_ui_components/cosmos_text_theme.dart';
 import 'package:cosmos_ui_components/cosmos_theme.dart';
 import 'package:cosmos_ui_components/utils/global_constants.dart';
 import 'package:flutter/foundation.dart';
@@ -10,12 +11,16 @@ class CosmosOutlineButton extends StatelessWidget {
     this.text = '',
     this.height = GlobalConstants.defaultButtonHeight,
     this.suffixIcon,
+    this.backgroundColor,
+    this.textColor,
   }) : super(key: key);
 
   final VoidCallback? onTap;
   final String text;
   final Widget? suffixIcon;
   final double height;
+  final Color? textColor;
+  final Color? backgroundColor;
 
   @override
   Widget build(BuildContext context) {
@@ -23,9 +28,9 @@ class CosmosOutlineButton extends StatelessWidget {
     return OutlinedButton(
       onPressed: onTap,
       style: OutlinedButton.styleFrom(
-        primary: theme.colors.text,
-        backgroundColor: theme.colors.background,
-        side: BorderSide(color: theme.colors.text),
+        primary: textColor ?? theme.colors.text,
+        backgroundColor: backgroundColor ?? theme.colors.background,
+        side: BorderSide(color: textColor ?? theme.colors.text),
         fixedSize: Size.fromHeight(height),
         shape: RoundedRectangleBorder(borderRadius: theme.borderRadiusM),
         elevation: 0,
@@ -33,7 +38,12 @@ class CosmosOutlineButton extends StatelessWidget {
       child: Row(
         mainAxisSize: MainAxisSize.min,
         children: [
-          Text(text),
+          Text(
+            text,
+            style: CosmosTextTheme.elevatedButton.copyWith(
+              color: theme.colors.text,
+            ),
+          ),
           if (suffixIcon != null) ...[
             SizedBox(width: theme.spacingS),
             suffixIcon!,
@@ -49,6 +59,8 @@ class CosmosOutlineButton extends StatelessWidget {
     properties
       ..add(ObjectFlagProperty<VoidCallback?>.has('onTap', onTap))
       ..add(StringProperty('text', text))
-      ..add(DoubleProperty('height', height));
+      ..add(DoubleProperty('height', height))
+      ..add(ColorProperty('backgroundColor', backgroundColor))
+      ..add(ColorProperty('textColor', textColor));
   }
 }
