ee871e7a91c178499fc29e0eaa24b04c94d95022
Andrzej Chmielewski <andrzejchm@gmail.com>
2022-06-21 11:18:22 +0200

    fix analysis

--- packages/cosmos_utils/test/address_hexing_test.dart
+++ packages/cosmos_utils/test/address_hexing_test.dart
@@ -6,6 +6,7 @@ void main() {
     const address = 'cosmos1ygptsc84j7lezp6cvrjffu52520tla508qp4vk';
     const expected = '2202b860f597bf91075860e494f28aa29ebff68f';
 
+    // ignore: deprecated_member_use_from_same_package
     expect(bech32ToHex(address), expected);
   });
 }
